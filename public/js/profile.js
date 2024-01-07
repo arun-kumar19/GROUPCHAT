b55dad575fa9c0d20b176c5ef903f5bb27a1f484
arun-kumar19 <arunmaurya1177@gmail.com>
2024-01-07 14:39:03 +0530

    Lets make the chat app realtime

--- public/js/profile.js
+++ public/js/profile.js
@@ -42,10 +42,17 @@ async function fetchmessage(){
     console.log('resonse:',response);
     const datalength=response.data.message.length;
     const data=response.data.message;
-    const tbody=document.getElementById("usermessage");
     //console.log('data:',data[i].message)
+    const table=document.getElementById("tabledata");
+    const tbody=document.createElement("tbody");
+    tbody.id="tbody";
+    table.appendChild(tbody)
     const tr=document.createElement("tr");
     const td=document.createElement("td");
+    td.textContent="You Joined";
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+
     td.textContent="You Joined";
     tr.appendChild(td);
     tbody.appendChild(tr);
@@ -59,10 +66,55 @@ async function fetchmessage(){
             tr.appendChild(td);
             tbody.appendChild(tr);
 
+        }
+    }
+    else{
+        const li=document.createElement("p");
+            li.textContent='no message';
+            ul.appendChild(li);
+    }
+    
+}
+
+setInterval(refreshMessages, 1000);
 
 
+async function refreshMessages(){
+    
+   
+    const table=document.getElementById("tabledata");
+    const tablebody=document.getElementById("tbody");
+    console.log('tablebody:',tablebody);
+    table.removeChild(tablebody)
+    const response=await axios.get('/fetchmessage',{headers:{'Authorization':localStorage.getItem("token")}});
 
+    console.log('resonse:',response);
+    const datalength=response.data.message.length;
+    const data=response.data.message;
+    //console.log('data:',data[i].message)
 
+    const tbody=document.createElement("tbody");
+    tbody.id="tbody";
+    table.appendChild(tbody)
+    const tr=document.createElement("tr");
+    const td=document.createElement("td");
+    td.textContent="You Joined";
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+
+    td.textContent="You Joined";
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+
+    if(datalength>0){
+        
+        for(let i=0;i<datalength;i++){
+                console.log('data:',data[i].message)
+            const tr=document.createElement("tr");
+            const td=document.createElement("td");
+            td.textContent=data[i].message;
+            tr.appendChild(td);
+            tbody.appendChild(tr);
         }
     }
     else{
